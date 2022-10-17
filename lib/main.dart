6b506c3ceb8fe5dc7cb35bac6b9faacb6edef5e3
rakabgs27 <rakabgs2000@gmail.com>
2022-10-17 15:29:31 +0700

    Basic-Provider

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:testingprovider/person.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+   Provider(
+      create: (_) => Person(name: " Raka Bagas Fitriansyah", age: 20),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyNamePage(),
+    );
+  }
+}
+
+class MyNamePage extends StatelessWidget {
+  const MyNamePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Title'),
+      ),
+      body: Center(
+        child: 
+          Text(
+           '''
+          Hi ${Provider.of<Person>(context).name}!
+          You are ${Provider.of<Person>(context).age} years old''',
+          ),
+      ),
+    );
+  }
+}
